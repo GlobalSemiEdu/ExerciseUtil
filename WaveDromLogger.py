bb703f8b75be66eff7c3a8d4ec7165d7659e5777
Juhee Choi <85562795+GlobalSemiEdu@users.noreply.github.com>
2025-06-02 08:20:11 +0900

    Update WaveDromLogger.py

--- WaveDromLogger.py
+++ WaveDromLogger.py
@@ -1,43 +1,55 @@
-class WaveDromLogger:
+class WaveformLogger:
     def __init__(self):
         self.log = {}
         self.prev = {}
-        for sig in ["PCLK"] + CONTROL_SIGS:
+        self.clk_sig = None
+        self.ctrl_sigs = None
+        self.data_sigs = None
+
+    def init_signals(self, signals):
+        self.clk_sig = signals.ClkSig
+        self.ctrl_sigs = signals.CtrlSigs
+        self.data_sigs = signals.DataSigs
+
+        for sig in [self.clk_sig] + self.ctrl_sigs:
             self.log[sig] = ""
             self.prev[sig] = None
-        for sig in DATA_SIGS:
-            self.log[sig] = ["",[]]
+
+        for sig in self.data_sigs:
+            self.log[sig] = ["", []]
             self.prev[sig] = None
+
         self.cycle = 0
 
-    def record(self, sig):
-        # PCLK
-        clk = "P" if logger.cycle % 2 == 0 else "."
-        self.log["PCLK"] += str(clk)
-        logger.cycle += 1
+    def record(self, current_signals):
+        clk = "P" if self.cycle % 2 == 0 else "."
+        self.log[self.clk_sig] += str(clk)
+        self.cycle += 1
 
-        for field in CONTROL_SIGS:
-            if self.prev[field] == sig[field]:
-                self.log[field] += "."
+        for sig in self.ctrl_sigs:
+            if self.prev[sig] == current_signals[sig]:
+                self.log[sig] += "."
             else:
-                self.log[field] += str(sig[field])
-                self.prev[field] = sig[field]
+                self.log[sig] += str(current_signals[sig])
+                self.prev[sig] = current_signals[sig]
 
-        for field in DATA_SIGS:
-            if self.prev[field] == sig[field]:
-                self.log[field][0] += "."
+        for sig in self.data_sigs:
+            if self.prev[sig] == current_signals[sig]:
+                self.log[sig][0] += "."
             else:
-                self.log[field][0] += "3"
-                self.log[field][1].append(hex(sig[field]).upper())
-                self.prev[field] = sig[field]
+                self.log[sig][0] += "3"
+                self.log[sig][1].append(hex(current_signals[sig]).upper())
+                self.prev[sig] = current_signals[sig]
 
     def to_json(self):
         wavedrom_list = []
-        for field in ["PCLK"]+CONTROL_SIGS:
-            signal_entry = {"name": field, "wave": self.log[field]}
+        for sig in [self.clk_sig] + self.ctrl_sigs:
+            signal_entry = {"name": sig, "wave": self.log[sig]}
             wavedrom_list.append(signal_entry)
-        for field in DATA_SIGS:
-            wave, data = self.log[field]
-            signal_entry = {"name": field, "wave": wave, "data": data}
+        for sig in self.data_sigs:
+            wave, data = self.log[sig]
+            signal_entry = {"name": sig, "wave": wave, "data": data}
             wavedrom_list.append(signal_entry)
         return {"signal": wavedrom_list}
+
+
